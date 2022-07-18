21c07876f8f515cdb3d484d08c07624852aa2919
surafel <72102647+surafel58@users.noreply.github.com>
2022-07-18 18:41:40 +0300

    Update Find Pivot Index.java

--- Find Pivot Index.java
+++ Find Pivot Index.java
@@ -1,27 +1,34 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        //variables to store the summation of left and right from the given index
-        int leftsum=0, rightsum=0;
-        //iterate through the array to find th epivot index
-        for(int i=0; i<nums.length; i++)
+        int[] prefixLeftSum = new int[nums.length], prefixRightSum = new int[nums.length];
+        
+        //if there is only one element then return its index
+        if(nums.length==1)
+            return 0;
+        
+        //initialize left prefix sum
+        prefixLeftSum[0] = nums[0];
+        
+        //store the left prefix sum of all elements
+        for(int i=1; i<prefixLeftSum.length; i++)
+            prefixLeftSum[i] = prefixLeftSum[i-1] + nums[i];
+        
+        //initialize right prefix sum
+        prefixRightSum[0] = prefixLeftSum[prefixLeftSum.length - 1];
+        
+        //store the right prefix sum of all elements
+        for(int i=1; i<prefixRightSum.length; i++)
+            prefixRightSum[i] = prefixRightSum[i-1] - nums[i-1];
+        
+        //iterate through the prefix sums and return the pivot index
+        for(int i=0; i<prefixRightSum.length; i++)
         {
-            //calculate the leftsum for the current index
-            for(int j=i-1; j!=-1; j--)
-                leftsum+=nums[j];
-            
-            //calculate the rightsum
-            for(int k=i+1; k<nums.length; k++)
-                rightsum+=nums[k];
-            
-            //if the left sum and right sum are equal for the current index then it is the pivot index
-            if(leftsum==rightsum)
+            //if left and right prefix sum of the givent element is the same, return its index
+            if(prefixLeftSum[i] == prefixRightSum[i])
                 return i;
-            
-            //reinitialize the leftsum and rightsum for the next index to be checked if it is pivot index
-            leftsum=0;
-            rightsum=0;
         }
-        //if not found return -1
+        
+        //return -1 if there is not pivot index
         return -1;
     }
 }
