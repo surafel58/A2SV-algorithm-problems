f1e8224564e1bda7f0dfce6e046acea0690c8820
surafel <72102647+surafel58@users.noreply.github.com>
2022-07-20 22:42:07 +0300

    Solution for Next Greater Element problem from leetcode

--- Next Greater Element I.java
+++ Next Greater Element I.java
@@ -1,26 +1,44 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        //result array
         int []ans = new int[nums1.length];
+        
         //set all elements of the ans to -1
         for(int i=0; i<nums1.length; i++)
             ans[i] = -1;
-        //itearte through nums1 to find the next greater element for each nums1[i]
+        
+        //monotonic stack(decreasing)
+        Stack<Integer> mono_stack = new Stack<>();
+        
+        //store next greater element of all elements 
+        int[] output = new int[nums2.length];
+        
+        //push index of first element 
+        mono_stack.push(0);
+        
+        //iterate through the array and assign the next greater element for each corresponding element 
+        for(int i=1; i<nums2.length; i++)
+        {
+            //if greater element is found and put it in the output array for the given index from the stack, and pop the index 
+            while(!mono_stack.empty() && nums2[mono_stack.peek()] < nums2[i])
+            {
+                output[mono_stack.peek()] = nums2[i];
+                mono_stack.pop();
+            }
+            
+            //push index 
+            mono_stack.push(i);
+        }
+        
+        //choose the needed ouput 
         for(int i=0; i<nums1.length; i++)
         {
-            //store the index of nums[i] from nums2 array
-            int index = indexOf(nums2,nums1[i]);
-            //iterate through nums2 array to find next greater element of the given element
-            for(int j=index; j<nums2.length-1; j++)
-            {
-                //if found put it in the ans array, and break out of the inner loop 
-                if(nums2[index]<nums2[j+1])
-                {
-                    ans[i] = nums2[j+1];
-                    break;
-                }    
-            }
+            int index = indexOf(nums2, nums1[i]);
+            if(output[index] != 0)
+                ans[i] = output[index];
         }
-        //return the array
+        
+        //return the result
         return ans;
     }
     
