ed6df9750e93360bcfc0e3b82727d8662ec669c7
surafel <72102647+surafel58@users.noreply.github.com>
2022-08-08 16:39:47 +0300

    Update Power of Four.java

--- Power of Four.java
+++ Power of Four.java
@@ -1,15 +1,38 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-         //use log4 n = x, if 4^x is n then return true
+        //use log4 n = x, if 4^x is n then return true
         //if n is zero return false
-        if(n==0)
-            return false;
+        // if(n==0)
+        //     return false;
         //calculate log n to base 4 and roundup
-	 	double x = Math.round(Math.log(n)/Math.log(4));
+	 	// double x = Math.round(Math.log(n)/Math.log(4));
         //if n equals to 4 the power of x return true
-	 	if(n == Math.pow(4, x))
-	 		return true;
+	 	// if(n == Math.pow(4, x))
+	 		// return true;
         //otherwise return false
-	 	return false;
+	 	// return false;
+        
+        //call the recursive method and return its result
+        return checkNumber(Double.valueOf(n));
+    }
+    
+    
+    public boolean checkNumber(Double n)
+    {
+        //if n is less than or equal to 0 then it cant be power of four
+        if(n<=0)
+            return false;
+        
+        //base case - if n is 1 which means its trvially true
+        if(n==1)
+            return true;
+        
+        //if n is not integer - then n cant be power of four
+        else if((n%1)!=0)
+            return false;
+        
+        //else keep dividing n by 4 - recursively call itself with n/4 input
+        else 
+            return checkNumber(n/4);
     }
 }
