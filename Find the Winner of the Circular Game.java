e01215c94837bb04df584023e27006586d637760
surafel <72102647+surafel58@users.noreply.github.com>
2022-09-13 14:08:19 +0300

    Update Find the Winner of the Circular Game.java

--- Find the Winner of the Circular Game.java
+++ Find the Winner of the Circular Game.java
@@ -55,3 +55,34 @@ class Solution {
         return x;
     }
 }
+
+
+
+//BETTER SOLUTION recursive
+class Solution {
+    public int findTheWinner(int n, int k) {
+        
+        if(k == 1)
+            return n;
+        
+        if(n==1)
+            return 0;
+        
+        
+        return winner(n, k) + 1;
+        
+    }
+    
+    public int winner(int n, int k)
+    {
+        if(n == 1)
+            return 0;
+        else
+        {
+            //winner for n == winner for n-1 with offset k, and if result is greater than current n, it will start again at 0 (result % n)
+            //works for when n = 0, so final result is for n starting from 0, which means add 1 to the final result
+            int result = (winner(n-1, k) + k)%n; 
+            return result;
+        }
+    }
+}
