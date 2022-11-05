c0ccc6864f120536647ffd2fdb732541c4498e95
surafel <72102647+surafel58@users.noreply.github.com>
2022-11-05 20:13:13 +0300

    Update Car Pooling.java

--- Car Pooling.java
+++ Car Pooling.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        // return false;
+        
+        ///Brute force approach!!
+        
         java.util.Arrays.sort(trips, new java.util.Comparator<int[]>() {
             public int compare(int[] a, int[] b) {
                 return Integer.compare(a[1], b[1]);
@@ -31,6 +33,48 @@ class Solution {
                 else
                     return false;
             }
+        }
+        return true;
+    }
+}
+
+
+
+
+///Efficient Approach
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+
+        //max element
+        int max = 0;
+        for(int i=0; i < trips.length; i++)
+        {
+            if(trips[i][2] > max)
+                max = trips[i][2];
+        }
+        
+        //I used line sweep method - prefixSum
+        int[] positions = new int[max + 1];
+
+        for(int i=0; i < trips.length; i++)
+        {
+            int start = trips[i][1];
+            int end = trips[i][2];
+
+            positions[start] += trips[i][0];
+
+            if(end < positions.length)
+                positions[end] -= trips[i][0];
+        }
+
+        for(int i=1; i < positions.length; i++)
+            positions[i] += positions[i-1];
+
+        for(int i=0; i<positions.length; i++)
+        {
+            //if number of passengers on that position is > capacity then return false
+            if(positions[i] > capacity)
+                return false;
         }
         return true;
     }
